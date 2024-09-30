4aec55cc498c147de78ab3db65a198b5ce00d40d
MajorGKing <majorgking@gmail.com>
2024-09-30 11:47:41 +0900

    샘플 프로젝트 업로드

---
+++ Assets/Scripts/BackgroundLoop.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UniRx;
+using UniRx.Triggers;
+
+// 왼쪽 끝으로 이동한 배경을 오른쪽 끝으로 재배치하는 스크립트
+public class BackgroundLoop : MonoBehaviour
+{
+    private float width; // 배경의 가로 길이
+
+    private void Awake()
+    {
+        // BoxCollider2D 컴포넌트의 Size 필드의 X 값을 가로 길이로 사용
+        BoxCollider2D backgroundCollider = GetComponent<BoxCollider2D>();
+        width = backgroundCollider.size.x;
+    }
+
+    private void Start()
+    {
+        this.UpdateAsObservable()
+        .Where(_ => transform.position.x <= -width)
+        .Subscribe(_ =>
+        {
+            Vector2 offset = new Vector2(width * 2f, 0);
+            transform.position = (Vector2)transform.position + offset;
+        });
+    }
+}
---
+++ Assets/Scripts/Platform.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UniRx;
+using UniRx.Triggers;
+
+// 발판으로서 필요한 동작을 담은 스크립트
+public class Platform : MonoBehaviour
+{
+    public GameObject[] obstacles; // 장애물 오브젝트들
+    private bool stepped = false; // 플레이어 캐릭터가 밟았었는가
+
+    private void Start()
+    {
+        this.OnEnableAsObservable()
+        .Subscribe(_ =>
+        {
+            stepped = false;
+
+            for (int i = 0; i < obstacles.Length; i++)
+            {
+                // 현재 순번의 장애물을 1/3의 확률로 활성화
+                if (Random.Range(0, 3) == 0)
+                {
+                    obstacles[i].SetActive(true);
+                }
+                else
+                {
+                    obstacles[i].SetActive(false);
+                }
+            }
+        });
+
+        this.OnCollisionEnter2DAsObservable()
+        .Where(col => col.collider.tag == "Player" && stepped == false)
+        .Subscribe(_ =>
+        {
+            stepped = true;
+            GameManager.instance.AddScore(1);
+        });
+    }
+}
